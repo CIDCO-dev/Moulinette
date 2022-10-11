b92c24c01e0a2dfa2672fa42b1687fc86fb0a505
Patrick Charron-Morneau <patrick.cmorneau@cidco.ca>
2022-10-11 13:20:59 -0400

    better params for raster generation

--- script/rasterize.bash
+++ script/rasterize.bash
@@ -21,35 +21,33 @@ echo "<OGRVRTDataSource>
     <OGRVRTLayer name=\"${FILENAME%.*}\">
         <SrcDataSource>$dataSource</SrcDataSource>
         <GeometryType>wkbPoint</GeometryType>
-        <GeometryField encoding=\"PointFromColumns\" x=\"field_1\" y=\"field_2\" z=\"field_3\"/>
+        <GeometryField encoding=\"PointFromColumns\" x=\"field_2\" y=\"field_1\" z=\"field_3\"/>
     </OGRVRTLayer>
 </OGRVRTDataSource>" > $VRT
 
 
 
 interpolations=("invdist" "average" "nearest" "linear")
-#powers=("1.1" "1.2" "1.4" "1.6" "1.8" "2" "2.2" "2.4" "2.6" "2.8" "3")
-#smoothings=("1" "1.5" "2" "2.5" "3" "4" "5" "10")
-powers=("1.1" "2.0")
-smoothings=("1.0" "2.0")
+powers=("1.0" "1.2" "1.3" "1.5" "1.6" "1.8" "2.0" "2.2" "2.4" "2.6" "2.8" "3.0")
+smoothings=("0")
 for interpolationAlgo in ${interpolations[@]}; do
 	if [[ "$interpolationAlgo" == "invdist" ]]; then
 		# default parameter
 		layerName="${FILENAME%.*}"
-		rasterName="${layername}_${interpolationAlgo}.tiff"
+		rasterName="${layerName}_${interpolationAlgo}.tiff"
 		gdal_grid -zfield "field_3" -a_srs EPSG:$epsgCode -a $interpolationAlgo -ot Float64 -l $layerName $VRT $outputPath/$rasterName --config GDAL_NUM_THREADS ALL_CPUS
 		for power in ${powers[@]}; do
 			# echo $power
 			for smoothing in ${smoothings[@]}; do
 				# custom parameter
 				layerName="${FILENAME%.*}"
-				rasterName="${layername}_${interpolationAlgo}_p${power}_s${smoothing}.tiff"
+				rasterName="${layerName}_${interpolationAlgo}_p${power}_s${smoothing}.tiff"
 				gdal_grid -zfield "field_3" -a_srs EPSG:$epsgCode -a $interpolationAlgo:power=$power:smoothing=$smoothing -ot Float64 -l $layerName $VRT $outputPath/$rasterName --config GDAL_NUM_THREADS ALL_CPUS
 			done
 		done
 	else
 		layerName="${FILENAME%.*}"
-		rasterName="${layername}_${interpolationAlgo}.tiff"
+		rasterName="${layerName}_${interpolationAlgo}.tiff"
 		gdal_grid -zfield "field_3" -a_srs EPSG:$epsgCode -a $interpolationAlgo -ot Float64 -l $layerName $VRT $outputPath/$rasterName --config GDAL_NUM_THREADS ALL_CPUS
 	fi
 done
